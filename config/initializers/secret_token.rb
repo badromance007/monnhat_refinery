d7f8ed17ff1da2d7b78e5d9700274563c0e8ba4b
Tuong Nguyen <nguyentrongtuong1993@yahoo.com>
2014-08-05 14:05:56 +0700

    Setup refinery the first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonnhatRefinery::Application.config.secret_token = '2462d24f159ff3e0ee5d05d0277dd3632faee8496160540e7ba48cd1ae277408667b24ce8640b25a7158528b1be545c0bf0d98bf42bdb9b5fb47193410404bb2'
